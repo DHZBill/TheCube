87cc6dc067c98cf7d8de92ac950dd9dd900868cd
adbforlife <adbforlife2018@gmail.com>
2017-06-22 11:59:51 -0700

    Add basic timer

---
+++ Assets/Components/ScenePlay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenePlay : MonoBehaviour {
+    private float timer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        int ms = (int)(((float)(timer - (int)timer)) * 1000);
+        int m = (int)timer / 60;
+        int s = (int)(timer - m * 60);
+
+        string timeString = m.ToString("D2") + ":" + s.ToString("D2") + ":" + ms.ToString("D3");
+
+        Debug.Log(timeString);
+    }
+}
