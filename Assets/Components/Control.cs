208175dc57f339a9dfbda24aaa03240c206cdb47
adbforlife <adbforlife2018@gmail.com>
2017-06-23 11:39:27 -0700

    Fix trail

--- Assets/Components/Control.cs
+++ Assets/Components/Control.cs
@@ -6,102 +6,123 @@ using UnityEngine;
 public class Control : MonoBehaviour
 {
 
-    public float speed;
-    public float moveSpeed = 1f;
-    public float sprintSpeed = 2f;
-    public float rotateTime = 1f;
-    float positionError = 0.05f;
-    Vector3 position;
-    Vector3 rotation;
-    bool turnCommand = false;
-    bool safeToTurn = false;
-    Vector3 rotate;
-    // Use this for initialization
-    void Start()
-    {
-        speed = moveSpeed;
-    }
+	public float speed;
+	public float moveSpeed = 1f;
+	public float sprintSpeed = 2f;
+	public float rotateTime = 1f;
+	float positionError = 0.05f;
+	Vector3 position;
+	Vector3 rotation;
+	bool turnCommand = false;
+	bool safeToTurn = false;
+	Vector3 rotate;
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(new Vector3(0, 0, Time.deltaTime * speed));
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            turnCommand = true;
-            rotate = new Vector3(0, 90f, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            turnCommand = true;
-            rotate = new Vector3(0, -90f, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            turnCommand = true;
-            rotate = new Vector3(-90f, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            turnCommand = true;
-            rotate = new Vector3(90f, 0, 0);
-        }
-        if (turnCommand)
-        {
-            checkPosition();
-            if (safeToTurn)
-            {
-                StartCoroutine(RotateMe(rotate, rotateTime));
-                turnCommand = false;
-            }
-        }
-        safeToTurn = false;
-    }
+	public GameObject trailSpawn;
+	public Transform trail;
+	public Transform trailPrefab;
+	private Vector3 intPosition;
+	private Vector3 newPosition;
+	// Use this for initialization
+	void Start()
+	{
+		speed = moveSpeed;
+	}
 
-    IEnumerator RotateMe(Vector3 byAngles, float inTime)
-    {
-        GameObject temp = new GameObject("temp");
-        temp.transform.position = transform.position;
-        temp.transform.rotation = transform.rotation;
-        temp.transform.Rotate(rotate);
-        Freeze();
-        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
-        {
-            transform.rotation = (Quaternion.Lerp(transform.rotation, temp.transform.rotation, t));
-            yield return null;
-        }
-        Destroy(temp);
-        EulerAngleCorrection();
-        Resume();
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		int intX = (int)Mathf.Round(trailSpawn.transform.position.x);
+		int intY = (int)Mathf.Round(trailSpawn.transform.position.y);
+		int intZ = (int)Mathf.Round(trailSpawn.transform.position.z);
+		newPosition = new Vector3(intX, intY, intZ);
+		if (newPosition != intPosition)
+		{
+			Instantiate(trailPrefab, intPosition, Quaternion.identity, trail);
+			intPosition = newPosition;
+		}
+		else
+		{
+			intPosition = newPosition;
+		}
 
-    void Freeze()
-    {
-        speed = 0;
-    }
 
-    void Resume()
-    {
-        speed = moveSpeed;
-    }
-    void EulerAngleCorrection()
-    {
-        int factor = 90;
-        rotation = transform.eulerAngles;
-        transform.eulerAngles = new Vector3((float)Math.Round((rotation.x / (double)factor), MidpointRounding.AwayFromZero) * factor,
-                                            (float)Math.Round((rotation.y / (double)factor), MidpointRounding.AwayFromZero) * factor,
-                                            (float)Math.Round((rotation.z / (double)factor), MidpointRounding.AwayFromZero) * factor
-                                            );
-    }
-    void checkPosition()
-    {
-        Vector3 pos = transform.position;
-        Vector3 finalPos = new Vector3((float)Math.Ceiling(pos.x), (float)Math.Ceiling(pos.y), (float)Math.Ceiling(pos.z));
-        if (Vector3.Distance(pos, finalPos) < positionError)
-        {
-            transform.position = finalPos;
-            safeToTurn = true;
-        }
-    }
+		transform.Translate(new Vector3(0, 0, Time.deltaTime * speed));
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			turnCommand = true;
+			rotate = new Vector3(0, 90f, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			turnCommand = true;
+			rotate = new Vector3(0, -90f, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			turnCommand = true;
+			rotate = new Vector3(-90f, 0, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			turnCommand = true;
+			rotate = new Vector3(90f, 0, 0);
+		}
+		if (turnCommand)
+		{
+			checkPosition();
+			if (safeToTurn)
+			{
+				StartCoroutine(RotateMe(rotate, rotateTime));
+				turnCommand = false;
+			}
+		}
+		safeToTurn = false;
+	}
+
+	IEnumerator RotateMe(Vector3 byAngles, float inTime)
+	{
+		GameObject temp = new GameObject("temp");
+		temp.transform.position = transform.position;
+		temp.transform.rotation = transform.rotation;
+		temp.transform.Rotate(rotate);
+		Freeze();
+		for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+		{
+			transform.rotation = (Quaternion.Lerp(transform.rotation, temp.transform.rotation, t));
+			yield return null;
+		}
+		Destroy(temp);
+		EulerAngleCorrection();
+		Resume();
+	}
+
+	void Freeze()
+	{
+		speed = 0;
+	}
+
+	void Resume()
+	{
+		speed = moveSpeed;
+	}
+	void EulerAngleCorrection()
+	{
+		int factor = 90;
+		rotation = transform.eulerAngles;
+		transform.eulerAngles = new Vector3((float)Math.Round((rotation.x / (double)factor), MidpointRounding.AwayFromZero) * factor,
+			(float)Math.Round((rotation.y / (double)factor), MidpointRounding.AwayFromZero) * factor,
+			(float)Math.Round((rotation.z / (double)factor), MidpointRounding.AwayFromZero) * factor
+		);
+	}
+	void checkPosition()
+	{
+		Vector3 pos = transform.position;
+		Vector3 finalPos = new Vector3((float)Math.Ceiling(pos.x), (float)Math.Ceiling(pos.y), (float)Math.Ceiling(pos.z));
+		if (Vector3.Distance(pos, finalPos) < positionError)
+		{
+			transform.position = finalPos;
+			safeToTurn = true;
+		}
+	}
 }
 
